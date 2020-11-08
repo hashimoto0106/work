512ed09c8380ad8b9bca91edfa53ba92007847e3
hashimoto naoki <hashimoto0106@gmail.com>
2020-11-08 13:08:20 +0900

    file name

--- cppcheck/cppcheck_exe.bat
+++
@@ -1,4 +0,0 @@
-rem This works if run as administrator - otherwise no files are created
-set GCOVEXE="C:\Program Files\Cppcheck\cppcheck.exe"
-%GCOVEXE% --enable=all --template={file},{line},{severity},{id},\"{message}\" --project=./ACU.cppcheck --output-file=.\cppcheck_result.csv
-pause
